372d1a93fd279b1aa934aca39a3d208aa1f700ec
Jan Tacakiewicz <jtacakiewicz@internships.antmicro.com>
2025-07-30 15:47:05 +0200

    [#80850] tae: logger: found the solution to all problems with logs being blocked

--- autoPyTorch/evaluation/tae.py
+++ autoPyTorch/evaluation/tae.py
@@ -356,7 +356,7 @@ class ExecuteTaFuncWithQueue(AbstractTAFunc):
             # Pynisher expects seconds as a time indicator
             wall_time_in_s=int(cutoff) if cutoff is not None else None,
             mem_in_mb=self.memory_limit,
-            capture_output=True,
+            capture_output=False,
             context=context,
         )
 
